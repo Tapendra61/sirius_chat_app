c8b3e1e1a96af5cda2b98ba5fbb0cc3525cab7eb
Tapendra Shahi <tapendrashahi61@gmail.com>
2025-02-28 23:39:26 +0545

    Implemented API: Update Profile

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -97,7 +97,12 @@ export const updateProfile = async (req, res) => {
 		}
 
 		const uploadResponse = await cloudinary.uploader.upload(profilePic);
+		const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true });
+
+		res.status(200).json(updatedUser);
 	} catch (error) {
+		console.log("Error in update profile: ", error);
+		res.status(500).json({ message: "Internal server error" });
 
 	}
 }
