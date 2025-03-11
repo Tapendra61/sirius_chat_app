cf8593f8b0bb7f07238218c19b34c8125a447e3d
Tapendra Shahi <tapendrashahi61@gmail.com>
2025-03-11 15:28:19 +0545

    Implemented: sendMessage controller

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar } from "../controllers/message.controller.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const messageRoutes = express.Router();
 
 messageRoutes.get("/users", protectRoute, getUsersForSidebar);
 messageRoutes.get("/:id", protectRoute, getMessages);
+messageRoutes.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRoutes;
