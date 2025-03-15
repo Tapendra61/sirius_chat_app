0a6eaed6fbb4d62b3dcd523d3d4506b1aeff6508
Tapendra Shahi <tapendrashahi61@gmail.com>
2025-03-15 16:55:39 +0545

    AuthImagePattern decoration impl

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore';
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from 'lucide-react';
 import { Link } from "react-router-dom";
+import AuthImagePattern from '../components/AuthImagePattern';
 
 const SignUpPage = () => {
 	const [showPassword, setShowPassword] = useState(false);
@@ -137,6 +138,12 @@ const SignUpPage = () => {
 					</div>
 				</div>
 			</div>
+
+			{/* Right Side */}
+			<AuthImagePattern
+				title="Join our community"
+				subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+			/>
 		</div>
 	)
 }
