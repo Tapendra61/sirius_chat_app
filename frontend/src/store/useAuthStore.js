982a2fd43bd3c44137bf95d0dfdb9ac46a2e334b
Tapendra Shahi <tapendrashahi61@gmail.com>
2025-03-21 15:40:32 +0545

    Profile Uploading Frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -40,13 +40,13 @@ export const useAuthStore = create((set) => ({
 		set({ isLoggingIn: true });
 		try {
 			const res = await axiosInstance.post("/auth/login", data);
-			set({authUser: res.data});
+			set({ authUser: res.data });
 			toast.success("Logged in successfully");
 		} catch (error) {
 			toast.error(error.response.data.message);
 		}
 		finally {
-			set({isLoggingIn: false});
+			set({ isLoggingIn: false });
 		}
 	},
 
@@ -59,4 +59,8 @@ export const useAuthStore = create((set) => ({
 			toast.error(error.response.data.message);
 		}
 	},
+
+	updateProfile: async (data) => {
+		
+	}
 }));
