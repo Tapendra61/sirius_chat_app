f38e011244899672cb123ce12141e3f0642a2789
Tapendra Shahi <tapendrashahi61@gmail.com>
2025-03-21 16:06:45 +0545

    Profile uploading feature

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 import { Camera, Mail, User } from 'lucide-react';
 
 const ProfilePage = () => {
 	const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+	const [selectedImage, setSelectedImage] = useState(null);
 	const handleImageUpload = async (e) => {
+		const file = e.target.files[0];
+		if(!file) return;
 
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = async() => {
+			const base64Image = reader.result;
+			setSelectedImage(base64Image);
+			await updateProfile({profilePic: base64Image});
+		};
 	};
 	return (
 		<div className='h-screen pt-20'>
@@ -19,7 +29,7 @@ const ProfilePage = () => {
 					{/* avatar upload section */}
 					<div className='flex flex-col items-center gap-4'>
 						<div className='relative'>
-							<img src={authUser.profilePic || "/avatar.png"} alt="profile"
+							<img src={selectedImage || authUser.profilePic || "/avatar.png"} alt="profile"
 								className='size-32 rounded-full object-cover border-4'
 							/>
 							<label htmlFor="avatar-upload"
