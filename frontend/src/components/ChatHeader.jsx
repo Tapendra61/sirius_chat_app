4cc0b116492ac5230dc079f1240f4dd8d53f37f6
Tapendra <tapendrashahi61@gmail.com>
2025-08-23 20:18:26 +0545

    Completed Chat Header

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -19,7 +19,17 @@ const ChatHeader = () => {
 					</div>
 
 					{/* User info */}
+					<div>
+						<h3 className="font-medium">{selectedUser.fullName}</h3>
+						<p className="text-sm text-base-content/70">
+							{onlineUsers.includes(selectedUser._id) ? "Online" : "Offline"}
+						</p>
+					</div>
 				</div>
+
+				<button onClick={() => setSelectedUser(null)}>
+					<X />
+				</button>
 			</div>
 		</div>
 	)
