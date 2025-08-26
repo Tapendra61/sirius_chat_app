ea111a55ac40be33240915afe373aeaef3b2e382
Tapendra <tapendrashahi61@gmail.com>
2025-08-26 18:42:34 +0545

    Implemented Send Message Image Preview

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import { useChatStore } from "../store/useChatStore.js";
+import { X } from "lucide-react";
 
 const MessageInput = () => {
 	const [text, setText] = useState("");
@@ -16,15 +17,33 @@ const MessageInput = () => {
 
 	};
 
-	const handleSendMessage = async(e) => {
+	const handleSendMessage = async (e) => {
 
 	};
 
 	return (
 		<div className="p-4 w-full">
+			{
+				imagePreview && (
+					<div className="mb-3 flex items-center gap-2">
+						<div className="relative">
+							<img src={imagePreview} alt="Preview"
+								className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+							/>
+						</div>
+						<button onClick={removeImage}
+							className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300 flex items-center justify-center"
+							type='button'
+						>
+							<X className="size-3" />
+						</button>
+					</div>
+				)
+			}
 
+			
 		</div>
-	)
-}
+	);
+};
 
 export default MessageInput
