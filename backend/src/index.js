aa8af06f34104464c311ff370148eff9fa08825a
Tapendra Shahi <tapendrashahi61@gmail.com>
2026-03-29 15:59:39 +0545

    Changed default image send size allowed to 5 mb

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(cors(
 	{
